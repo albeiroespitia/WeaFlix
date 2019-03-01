aa756b64607717093d4eb7e05fbf613bb09a9e9e
Albeiro <albeiroespitiasierra@gmail.com>
2019-02-28 21:27:06 -0500

    Añadiendo variables mongodb

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://albeiro:DEVELOPisamazing123@ds125892.mlab.com:25892/weaflixdb'
+    db: process.env.MONGODB || 'mongodb://albeiro:telameto123@ds125892.mlab.com:25892/weaflixdb'
 }
